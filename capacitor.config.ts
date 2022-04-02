e15ff34d4549b25df667588b3632a068e458306e
mafealno <mafealno@gmail.com>
2022-04-02 00:19:58 -0300

    build: Adicionado pacote do capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cronog',
+  appName: 'cronog-client',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;

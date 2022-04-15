07884531811a5c61572627bb0acd0bf3915d38c4
mafealno <mafealno@gmail.com>
2022-04-14 21:54:40 -0300

    add: config app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,18 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cronog',
   appName: 'cronog-client',
-  webDir: 'public',
-  bundledWebRuntime: false
+  webDir: 'build',
+  bundledWebRuntime: false,
+  server: {
+    url: "http://192.168.0.174:3000"
+  },
+  plugins: {
+    Keyboard: {
+      resize: "ionic",
+      style: "LIGHT",
+      resizeOnFullScreen: false,
+    }
+  }
 };
 
 export default config;

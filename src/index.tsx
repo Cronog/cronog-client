29a597fbcea133521c16e8e3cb3a19f84206ef36
mafealno <mafealno@gmail.com>
2022-04-14 21:52:26 -0300

    alter: adicionado Toast e roteamento

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './pages/Main';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+import Toast from './components/Toast';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toast />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

abf49e3e24c1b571723810abe38d4ab224a90d5b
mafealno <mafealno@gmail.com>
2022-04-20 11:41:37 -0300

    add utils cronog

---
+++ src/utils/cronog.ts
@@ -0,0 +1,29 @@
+import { Cronog } from "../types/Cronog";
+import { Response } from "../types/Response";
+import { getCredentials } from "./auth";
+import { chamarBackEnd } from "./backend";
+
+export const getCronogById = async (id: string) : Promise<Response<Cronog>> => {
+    const response = await chamarBackEnd<Cronog>("GET", `/cronog/${getCredentials()?.uid}/${id}`)
+    return response;
+}
+
+export const getCronogByUserId = async () : Promise<Response<Cronog[]>> => {
+    const response = await chamarBackEnd<Cronog[]>("GET", `/cronog/${getCredentials()?.uid}`)
+    return response;
+}
+
+export const saveCronog = async (cronog : Cronog) : Promise<Response<any>> => {
+    const response = await chamarBackEnd("POST", "/cronog", cronog);
+    return response;
+}
+
+export const updateCronog = async (cronog : Cronog, id : string) : Promise<Response<any>> => {
+    const response = await chamarBackEnd("PUT", `/cronog/${getCredentials()?.uid}/${id}`, cronog);
+    return response;
+}
+
+export const deleteCronog = async (id : string | undefined) : Promise<Response<any>> => {
+    const response = await chamarBackEnd("DELETE", `/cronog/${getCredentials()?.uid}/${id}`);
+    return response;
+}

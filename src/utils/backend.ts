36322904b05d68d3ad1d8ee85a8860c5310385e4
mafealno <mafealno@gmail.com>
2022-08-13 00:33:21 -0300

    feat: Criado para quando o token de acesso expirar, fazer uma nova requisição para gerar novo token de acesso e fazer a requisição original novamente.

--- src/utils/backend.ts
+++ src/utils/backend.ts
@@ -1,68 +1,35 @@
 import { showToast } from "../components/Toast/Toast";
 import { RequestType } from "../types/RequestType";
 import Response from "../types/Response";
-import { getCredentials, singOut } from "./auth";
+import { getCredentials, renewAccessToken, singOut } from "./auth";
 
 const linkBackEnd = process.env.REACT_APP_API_ENVIRONMENT ? process.env.REACT_APP_API_URL_DEV : process.env.REACT_APP_API_URL;
 
-export async function getBackEnd<T = {}>(method : string, path : string, body? : any, contentType: RequestType = 0) : Promise<Response<T>> {
+export async function getBackEnd<T = {}>(method : string, path : string, body? : any, contentType: RequestType = RequestType.json) : Promise<Response<T>> {
 
-    let headers;
+    let headers = { "tokenAuth" : getCredentials()?.accessToken || "" }
 
-    if(contentType == 1){
-        headers = {
-            "tokenAuth" : getCredentials()?.stsTokenManager.accessToken || ""
-            }
-        }else{
-        headers = {
-            "Content-Type": "application/json",
-            "tokenAuth" : getCredentials()?.stsTokenManager.accessToken || ""
-            }
-        }
+    const response = await fetch(linkBackEnd + path, {
+        method: method,
+        headers: headers,
+        body: contentType == RequestType.formData ? body : JSON.stringify(body),
+        })
 
-    // try {
-        if(body){
-            const response = await fetch(linkBackEnd + path, {
-                method: method,
-                headers: headers,
-                body: contentType == 1 ? body : JSON.stringify(body),
-                })
-            return resolveResponse(await response.json())
+    const data = await response.json()
+
+    if(data.status == 401){
+        await renewAccessToken();
+        await getBackEnd(method, path, body, contentType);
+    }
     
-        }else{
-            const response = await fetch(linkBackEnd + path, {
-                method: method,
-                headers: headers,
-                })
-            return resolveResponse(await response.json())
-            }
-        // } catch (error) {
-        //     if(error == "TypeError: Failed to fetch"){
-        //         return resolveResponse({
-        //             status: 510,
-        //             message: "Erro inesperado",
-        //             success: false,
-        //             data: error
-        //         } as Response<T>)
-        //     }else{
-        //         return resolveResponse({
-        //             status: 511,
-        //             message: "Erro não tratado",
-        //             success: false,
-        //             data: error
-        //         } as Response<T>)
-        //     }
-        // }
+    return resolveResponse(data)
   };
 
-  function resolveResponse<T = {}>(response : Response<T>) : Response<T> {
-      switch(response.status){
-        case 509:
+function resolveResponse<T = {}>(response : Response<T>) : Response<T> {
+    switch(response.status){
+        case 403:
             singOut()
             break;
-        case 510:
-            showToast("error", response.message);
-            break;
-      }
-      return response
+    }
+    return response
   } 
